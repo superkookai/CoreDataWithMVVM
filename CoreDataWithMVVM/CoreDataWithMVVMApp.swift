ae483c993b6fbf2dac7febfc1b6ee5f385c2a6d6
Weerawut Chaiyasomboon <weerawut@live.com>
2024-01-29 11:17:42 +0700

    Initial Commit

---
+++ CoreDataWithMVVM/CoreDataWithMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataWithMVVMApp.swift
+//  CoreDataWithMVVM
+//
+//  Created by Weerawut Chaiyasomboon on 29/1/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataWithMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
